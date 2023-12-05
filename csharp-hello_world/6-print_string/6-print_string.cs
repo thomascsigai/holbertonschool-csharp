31bed57fd502c5839a825c208a0b9b4a6a6670c9
thomascsigai <thomascsigai@github.com>
2023-12-05 11:14:52 +0100

    Adding task 6 program

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine(String.Concat(str, str, str));
+		Console.WriteLine(str.Remove(9));
+	}
+}
