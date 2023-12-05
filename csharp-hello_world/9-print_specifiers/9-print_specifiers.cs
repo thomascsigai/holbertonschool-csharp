9659e4fc48bac437fe331e863beabbd4c2a88f66
thomascsigai <thomascsigai@github.com>
2023-12-05 15:11:16 +0100

    Adding task 9 program

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine(String.Format(new CultureInfo("en-US.utf-8"), "Percent: {0:p2}\nCurrency: {1:c}", percent, currency));
+	}
+}
