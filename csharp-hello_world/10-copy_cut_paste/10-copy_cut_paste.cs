c329651fe90285fe001fc00f45707489f0423137
thomascsigai <thomascsigai@github.com>
2023-12-05 18:13:39 +0100

    Adding task 10 program

---
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+			string word = "Holberton";
+			string word_first_3 = word.Remove(3);
+			string word_last_2 = word.Remove(0, word.Length-2);
+			string middle_word = word.Substring(1, word.Length-2);
+			Console.WriteLine("First 3 letters: {0}", word_first_3);
+			Console.WriteLine("Last 2 letters: {0}", word_last_2);
+			Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
