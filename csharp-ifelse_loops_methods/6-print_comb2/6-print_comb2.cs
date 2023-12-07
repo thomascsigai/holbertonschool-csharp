7da796ddb197c524c3a37f0dfa4968e6c2347667
thomascsigai <thomascsigai@github.com>
2023-12-07 17:44:07 +0100

    Adding task 6 program

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 9; i++)
+			for (int j = 1 + i; j < 10; j++)
+				if (i + j != 17)
+					Console.Write("{0}{1}, ", i, j);
+		Console.WriteLine("89");
+	}
+}
