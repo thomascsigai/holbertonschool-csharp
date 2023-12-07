aef97bb3c36ac23c0f176667eed779fa2a7c5858
thomascsigai <thomascsigai@github.com>
2023-12-07 17:26:24 +0100

    Adding task 3 program

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 'a'; i <= 'z'; i++)
+			if (i != 'q' && i != 'e')
+				Console.Write("{0}", (char)i);
+	}
+}
