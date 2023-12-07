09ff4329f7de15e2672d2c062eb608b931c001c3
thomascsigai <thomascsigai@github.com>
2023-12-07 18:22:40 +0100

    Adding task 11 program

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+	public static void PrintDiagonal(int length)
+	{
+		int i = 0;
+		do
+		{
+			Console.Write(length > 0 ? new String(' ', i++) + "\\\n" : "");
+		} while (i != length && length > 0);
+		Console.Write("\n");
+	}
+}
