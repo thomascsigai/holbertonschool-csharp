bc01203ce9d6508e999ebda1f852daef625d4fa6
thomascsigai <thomascsigai@github.com>
2023-12-07 13:37:19 +0100

    Using String methods, not string litterals

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-		str = "object-oriented programming in C#";
+		str = String.Concat(str.Split(' ')[8].Replace(',', ' '), str.Split(' ')[11], ' ', str.Split(' ')[17], ' ', str.Split(' ')[0]);
 		Console.WriteLine(str);
     }
 }
