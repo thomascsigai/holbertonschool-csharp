0f98c1a681cc402ea255ea71a3cd0394a7ddbd97
thomascsigai <thomascsigai@github.com>
2023-12-07 17:54:27 +0100

    Adding task 7 program

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{
+	public static bool IsLower(char c)
+	{
+		bool returnValue = false;
+		if (c >= 'a' && c <= 'z')
+			returnValue = true;
+		return returnValue;
+	}
+}
