870f8a1b3c5e87220c1b2ae9d8d7aaa1510a923b
thomascsigai <thomascsigai@github.com>
2023-12-12 15:31:34 +0100

    Adding task 4 program

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = Enumerable.Range(0, size).ToList<int>();
+        
+        for (int i = 0; i < size; i++)
+            Console.Write((i != 0) ? " {0}" : "{0}", newList[i]);
+        Console.WriteLine();
+        
+        return newList;
+    }
+}
