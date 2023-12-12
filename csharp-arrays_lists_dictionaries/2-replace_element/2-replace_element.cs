409480a72e6bb46bfd5be920bd86fa13f295ad8c
thomascsigai <thomascsigai@github.com>
2023-12-12 13:42:05 +0100

    Adding task 2 program

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index + 1 > array.Length)
+            Console.WriteLine("Index out of range");
+        else
+            array[index] = n;
+
+        return array;        
+    }
+}
