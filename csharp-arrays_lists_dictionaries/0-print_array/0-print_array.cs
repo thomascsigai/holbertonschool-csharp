3bf209c14716fc574e0d9d4cbe8a5399031c151f
thomascsigai <thomascsigai@github.com>
2023-12-12 12:24:53 +0100

    Adding task 0 program

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] newArray;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        
+        newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+            Console.Write(i == 0 ? "0" : " {0}", i);
+
+        Console.WriteLine();
+
+        return newArray;
+    }
+}
