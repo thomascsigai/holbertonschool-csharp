567778f99b78a8686f332447e0ee70f23a5e4673
thomascsigai <thomascsigai@github.com>
2023-12-12 15:03:14 +0100

    Adding task 3 program

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        for (int i = array.Length - 1; i > 0; i--)
+            Console.Write(array[i] + " ");
+        Console.WriteLine(array.Length > 0 ? array[0].ToString() : "");
+    }
+}
