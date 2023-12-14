297463e77f7a3e0d08fe5e112625f991f111d25a
thomascsigai <thomascsigai@github.com>
2023-12-14 16:34:51 +0100

    Adding task 2 program

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> isDivisibleList = new List<bool>();
+
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+                isDivisibleList.Add(true);
+            else
+                isDivisibleList.Add(false);
+        }
+
+        return isDivisibleList;
+    }
+}
