ed05428d6b3ef7860aa25bc029c844884d957fbd
thomascsigai <thomascsigai@github.com>
2023-12-14 16:15:03 +0100

    Adding task 5 program

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        return myList.Max();
+    }
+}
