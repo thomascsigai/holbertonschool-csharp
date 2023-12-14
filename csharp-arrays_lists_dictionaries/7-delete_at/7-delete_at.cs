3607a534bfb7b73e9476768a4ae9774dcae23bc9
thomascsigai <thomascsigai@github.com>
2023-12-14 16:49:41 +0100

    Adding task 7 program

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index + 1 > myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        for (int i = 0; i < length; i++)
+            if (i == index)
+                myList.Remove(myList[i]);
+
+        return myList;
+    }
+
+}
