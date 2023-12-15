c15c41b36c1809e73f86ef259133c6c8cbe0f553
thomascsigai <thomascsigai@github.com>
2023-12-15 14:45:03 +0100

    Fixing matrix first column

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -7,7 +7,7 @@ class Matrix
         int[,] newMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
 
         for (int i = 0; i < myMatrix.GetLength(0); i++)
-            for (int j = 1; j < myMatrix.GetLength(1); j++)
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
                 newMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
 
         return newMatrix;
