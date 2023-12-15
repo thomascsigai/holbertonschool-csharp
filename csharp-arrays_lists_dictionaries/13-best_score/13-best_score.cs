b27bc492f8af7b70ddf510fa54a70bec0d1d7722
thomascsigai <thomascsigai@github.com>
2023-12-15 14:14:36 +0100

    Adding task 13 program

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string keyMaxValue = "None";
+        int maxValue = 0;
+
+        foreach (KeyValuePair<string, int> element in myList)
+            if (element.Value > maxValue)
+            {
+                keyMaxValue = element.Key;
+                maxValue = element.Value;
+            }
+
+        return keyMaxValue;
+    }
+}
