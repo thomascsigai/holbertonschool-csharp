ca97caee45fe3bc51f35b13d02c39af5cc895b3f
thomascsigai <thomascsigai@github.com>
2023-12-15 14:05:14 +0100

    Adding task 12 program

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> newSortedList = new SortedList<string, string>();
+
+        foreach (KeyValuePair<string, string> element in myDict)
+            newSortedList.Add(element.Key, element.Value);
+
+        foreach (KeyValuePair<string, string> element in newSortedList)
+            Console.WriteLine("{0}: {1}", element.Key, element.Value);
+    }
+}
