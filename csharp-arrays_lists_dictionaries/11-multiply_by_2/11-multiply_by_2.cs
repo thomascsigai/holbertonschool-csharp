a6bc620a4e2e41bb972fac143e237036efc7e9b1
thomascsigai <thomascsigai@github.com>
2023-12-15 11:21:17 +0100

    Adding task 8, 9, 10, 11

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        foreach (KeyValuePair<string, int> element in myDict)
+            myDict[element.Key] = element.Value * 2;
+
+        return myDict;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/9-add_key_valu/9-add_key_valu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict.ContainsKey(key))
+            myDict[key] = value;
+        else
+            myDict.Add(key, value);
+
+        return myDict;
+    }
+}
