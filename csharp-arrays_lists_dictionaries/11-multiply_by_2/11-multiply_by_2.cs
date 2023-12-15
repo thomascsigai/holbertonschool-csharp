d367fb163698409c30db0bce6db30e49c1a28872
thomascsigai <thomascsigai@github.com>
2023-12-15 13:41:36 +0100

    Fixing task 11

--- csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Dictionary
 {
     public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
     {
-        foreach (KeyValuePair<string, int> element in myDict)
-            myDict[element.Key] = element.Value * 2;
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
 
-        return myDict;
+        foreach (KeyValuePair<string, int> element in myDict.ToList())
+            newDict[element.Key] = element.Value * 2;
+
+        return newDict;
     }
 }
