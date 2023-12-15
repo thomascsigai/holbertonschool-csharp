7288a986513ba139ee52f1b37c647466b653f0a1
thomascsigai <thomascsigai@github.com>
2023-12-15 14:31:32 +0100

    Adding task 14 program

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] newArray = new int[5,5];
+
+        newArray[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(newArray[i, 0]);
+            for (int j = 1; j < 5; j++)
+                Console.Write(" {0}", newArray[i, j]);
+            Console.WriteLine();
+        }
+    }
+}
