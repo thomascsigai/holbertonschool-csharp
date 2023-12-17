3f4f0d0b4ee2d7fbac70d176c3375686c1494a27
thomascsigai <thomascsigai@github.com>
2023-12-17 22:33:48 +0100

    Adding task 13 program

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index < 0 || index >= myLList.Count)
+            return;
+
+        LinkedListNode<int> current = myLList.First;
+
+        if (index == 0)
+        {
+            myLList.RemoveFirst();
+            return;
+        }
+
+        for (int i = 0; i < index - 1; i++)
+            current = current.Next;
+
+        myLList.Remove(current.Next);
+    }
+}
