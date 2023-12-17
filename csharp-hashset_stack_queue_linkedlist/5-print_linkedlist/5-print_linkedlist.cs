3a69d1554d42cbb141df369d60afc2d1f605a27e
thomascsigai <thomascsigai@github.com>
2023-12-17 15:45:43 +0100

    Handling the case where size is negative

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -6,7 +6,13 @@ class LList
 {
     public static LinkedList<int> CreatePrint(int size)
     {
-        LinkedList<int> newLList = new LinkedList<int>(Enumerable.Range(0, size).ToList());
+        LinkedList<int> newLList;
+
+        if(size >= 0) 
+            newLList = new LinkedList<int>(Enumerable.Range(0, size).ToList());
+        else 
+            newLList = new LinkedList<int>();
+        
         foreach (int element in newLList) Console.WriteLine($"{element}");
 
         return newLList;
