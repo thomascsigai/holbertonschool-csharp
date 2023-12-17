ff83b78ae48de72ce7db1796b3b0cbb5368157bf
thomascsigai <thomascsigai@github.com>
2023-12-17 14:45:25 +0100

    Adding task 2 program

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var uniqueToFirst = list1.Except(list2).ToList();
+        var uniqueToSecond = list2.Except(list1).ToList();
+
+        var result = new List<int>();
+        result.AddRange(uniqueToFirst);
+        result.AddRange(uniqueToSecond);
+
+        result.Sort();
+        return result;
+    }
+}
