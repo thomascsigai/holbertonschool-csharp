afad1cb45c6742963a9d96b46e44480e6e6757a3
thomascsigai <thomascsigai@github.com>
2023-12-17 15:17:41 +0100

    Adding task 4 program

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count != 0)
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Queue is empty");
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search) ? "True" : "False");
+
+        while (aQueue.Contains(search))
+            aQueue.Dequeue();
+
+        return aQueue;
+    }
+}
