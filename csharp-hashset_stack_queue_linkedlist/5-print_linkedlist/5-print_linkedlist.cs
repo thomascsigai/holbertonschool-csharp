0029e0197a3d9a05285cc4bbd46daf938467974b
thomascsigai <thomascsigai@github.com>
2023-12-17 15:39:53 +0100

    Adding task 5 program

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newLList = new LinkedList<int>(Enumerable.Range(0, size).ToList());
+        foreach (int element in newLList) Console.WriteLine($"{element}");
+
+        return newLList;
+    }
+}
