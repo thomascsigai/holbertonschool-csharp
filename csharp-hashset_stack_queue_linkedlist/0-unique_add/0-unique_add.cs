58c2dbd605a23d4650e06d823a1d2c3d012d62ae
thomascsigai <thomascsigai@github.com>
2023-12-17 14:23:32 +0100

    Adding task 0 program

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        return myList.Distinct().Sum();
+    }
+}
