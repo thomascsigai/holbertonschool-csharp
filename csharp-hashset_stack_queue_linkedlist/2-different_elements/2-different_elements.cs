4d938df51d6d69f67eaf8ea7ec6ff71e809107b9
thomascsigai <thomascsigai@github.com>
2023-12-17 14:52:43 +0100

    The program now doesn't use LINQ

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        var uniqueToFirst = list1.Except(list2).ToList();
-        var uniqueToSecond = list2.Except(list1).ToList();
+        List<int> newList = new List<int>();
 
-        var result = new List<int>();
-        result.AddRange(uniqueToFirst);
-        result.AddRange(uniqueToSecond);
+        foreach (int element in list1)
+            if (!list2.Contains(element))
+                newList.Add(element);
+        
+        foreach (int element in list2)
+            if (!list1.Contains(element) && !newList.Contains(element))
+                newList.Add(element);
 
-        result.Sort();
-        return result;
+        newList.Sort();
+        return newList;
     }
 }
