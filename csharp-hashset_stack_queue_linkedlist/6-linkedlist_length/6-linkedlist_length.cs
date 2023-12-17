43a403f58f21915eb21e6cfb5ece6631423bbae2
thomascsigai <thomascsigai@github.com>
2023-12-17 15:55:13 +0100

    Adding task 6 program

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int numberOfElements = 0;
+
+        foreach (var item in myLList)
+            numberOfElements++;
+        
+        return numberOfElements;
+    }
+}
