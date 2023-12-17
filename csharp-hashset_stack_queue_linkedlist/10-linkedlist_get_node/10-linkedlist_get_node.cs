ec19bb1ac4d67648dbbe2819820e235585f1019b
thomascsigai <thomascsigai@github.com>
2023-12-17 22:16:10 +0100

    Adding task 10 program

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+
+        if (n >= 0 && n < myLList.Count)
+            foreach (int item in myLList)
+                if (i++ == n)
+                    return item;
+                
+        return 0;
+    }
+}
