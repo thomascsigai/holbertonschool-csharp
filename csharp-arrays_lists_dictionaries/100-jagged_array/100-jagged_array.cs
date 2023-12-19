17482fb15571ba22f7a09b024e63d4bc48403ca3
thomascsigai <thomascsigai@github.com>
2023-12-19 12:20:34 +0100

    Adding task 16 program

---
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] newArray = new int[3][];
+
+        newArray[0] = new int [] {0, 1, 2, 3};
+        newArray[1] = new int [] {0, 1, 2, 3, 4, 5, 6};
+        newArray[2] = new int [] {0, 1};
+
+        foreach (int[] item in newArray)
+        {
+            Console.Write(item[0]);
+            for (int i = 1; i < item.Length; i++)
+            {
+                Console.Write($" {item[i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
