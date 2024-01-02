7db799a4ad492dfec749fd33809764b0d11ed46c
thomascsigai <thomascsigai@github.com>
2024-01-02 13:42:49 +0100

    Adding task 2 program

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> resultList = new List<int>(listLength);
+
+        for (int i = 0; i < listLength; i++)
+        {
+            try
+            {
+                resultList.Add(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                resultList.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+        
+        return resultList;
+    }
+}
