291c7bc9a1cf65e4788155f813716ba366322426
thomascsigai <thomascsigai@github.com>
2024-01-02 12:42:04 +0100

    Adding task 0 program

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            for (i = 0; i < n; i++)
+                Console.WriteLine("{0}", myList[i]);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            
+        }
+        
+        return i;
+        
+    }
+}
