e7e0c3d4028fb21af43315721bdebc9ea9551def
thomascsigai <thomascsigai@github.com>
2024-01-03 14:43:01 +0100

    Adding task 3 program

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        return $"Dog Name: {this.name}\nAge: {this.age}\nOwner: {this.owner}\nRating: {this.rating}";
+    }
+}
