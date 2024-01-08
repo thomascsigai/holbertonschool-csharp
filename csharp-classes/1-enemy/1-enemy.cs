932adf16851116b18268643357adb73d513b15f2
thomascsigai <thomascsigai@github.com>
2024-01-08 15:15:30 +0100

    Updating doc

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -8,7 +8,7 @@ namespace Enemies
         ///<summary>Health number bigger than 0</summary>
         public int health;
 
-        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
         public Zombie()
         {
             this.health = 0;
