d08a3c5d1ed5734a3d5e669a47f732371404a144
thomascsigai <thomascsigai@github.com>
2024-01-08 15:10:20 +0100

    Adding task 1 program

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a zombie</summary>
+    public class Zombie
+    {
+        ///<summary>Health number bigger than 0</summary>
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
