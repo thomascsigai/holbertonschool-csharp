5efe00ba033410511f3deb4c1fc9693360e34232
thomascsigai <thomascsigai@github.com>
2024-01-08 15:26:15 +0100

    Adding task 2 program

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a zombie.</summary>
+    public class Zombie
+    {
+        ///<summary>Health number bigger than 0.</summary>
+        public int health;
+
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        ///<param name="value">The instantiated zombie health greater than or equal 0.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                this.health = 0;
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            } 
+        }
+    }
+}
