ed761117ec8e9d4de05ad853e34c21c054a655df
thomascsigai <thomascsigai@github.com>
2024-01-08 19:49:44 +0100

    Adding task 2 files

---
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void RegularList_PositiveElements()
+        {
+            List<int> list = new List<int>() {1, 2, 3, 4};
+            int result;
+
+            result = MyMath.Operations.Max(list);
+
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void RegularList_NegativeElements()
+        {
+            List<int> list = new List<int>() {-1, -2, -3, -4};
+            int result;
+
+            result = MyMath.Operations.Max(list);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void EmptyList()
+        {
+            List<int> list = new List<int>() {};
+            int result;
+
+            result = MyMath.Operations.Max(list);
+
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    ///<summary>Defines arithmetic operations implementations.</summary>
+    public class Operations
+    {
+        ///<summary>Finds and returns max integer in a list of integers.</summary>
+        ///<param name="nums">The list.</param>
+        ///<returns>The max integer in the list.</returns>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+                return 0;
+
+            int max = nums[0];
+
+            foreach (int element in nums)
+            {
+                if (element > max)
+                    max = element;
+            }
+
+            return max;
+        }
+    }
+}
