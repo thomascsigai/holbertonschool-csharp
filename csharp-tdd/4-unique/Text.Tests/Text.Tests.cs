529595a622d6d4d68332d33a73c78ae192f22196
thomascsigai <thomascsigai@github.com>
2024-01-08 20:51:14 +0100

    Adding tests

--- csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -19,5 +19,29 @@ namespace Tests
 
             Assert.AreEqual(Text.Str.UniqueChar(str), -1);
         }
+
+        [Test]
+        public void FirstCharIsUnique()
+        {
+            string str = "accbb";
+
+            Assert.AreEqual(Text.Str.UniqueChar(str), 0);
+        }
+
+        [Test]
+        public void LastCharIsUnique()
+        {
+            string str = "ccbba";
+
+            Assert.AreEqual(Text.Str.UniqueChar(str), 4);
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            string str = "";
+
+            Assert.AreEqual(Text.Str.UniqueChar(str), -1);
+        }
     }
 }
