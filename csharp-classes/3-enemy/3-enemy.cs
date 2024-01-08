8c4f5e1621d232faab9c7b0797ea91179eadae1a
thomascsigai <thomascsigai@github.com>
2024-01-08 15:30:58 +0100

    Adding task 3 program

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents a zombie.</summary>
+    public class Zombie
+    {
+        ///Health number bigger than 0.
+        private int health;
+
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        ///<summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        ///<param name="value">The instantiated zombie health greater than or equal 0.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                this.health = 0;
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            } 
+        }
+
+        ///<summary>Gets the Zombie's health.</summary>
+        ///<returns>The value of health field</returns>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
