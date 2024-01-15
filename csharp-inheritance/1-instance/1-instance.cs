b96541342c22860cf425602a6c1b6bfb9c95c3e1
thomascsigai <thomascsigai@github.com>
2024-01-15 13:52:59 +0100

    Adding task 1 program

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+///<summary> Defines objects tools methods implementations.</summary>
+class Obj
+{
+    ///<summary>if the object is an instance of, or if the object is an instance of a class that inherited from, Array.</summary>
+    ///<param name="obj">The object to check</param>
+    ///<returns>True if the object is an Array, otherwise return False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
