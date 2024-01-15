bd7cacd2c00df40ec86ae57a3c57507924909fae
thomascsigai <thomascsigai@github.com>
2024-01-15 13:42:48 +0100

    Adding task 0 file

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+///<summary> Defines objects tools methods implementations.</summary>
+class Obj
+{
+    ///<summary>Tells if an object is type of int.</summary>
+    ///<param name="obj">The object to check</param>
+    ///<returns>True if the object is an int, otherwise return False.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
