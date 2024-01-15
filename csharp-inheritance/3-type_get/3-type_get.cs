50adbcb7be3df3f51958f1173ee07f749bb94653
thomascsigai <thomascsigai@github.com>
2024-01-15 14:37:22 +0100

    Adding task 3 program

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+///<summary> Defines objects tools methods implementations.</summary>
+class Obj
+{
+    ///<summary>Prints the names of the available properties and methods of an object.</summary>
+    ///<param name="myObj">The subclass.</param>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+        IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        
+        foreach (PropertyInfo p in pList)
+        {
+            Console.WriteLine(p.Name);
+        }
+        
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (MethodInfo m in mList)
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
