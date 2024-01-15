e33daef08185666717602ec7b4ab014cb0d10326
thomascsigai <thomascsigai@github.com>
2024-01-15 14:51:05 +0100

    Adding task 5 program

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary> Defines a Shape.</summary>
+class Shape
+{
+    ///<summary>Return the area of the shape.</summary>
+    ///<returns>The aera of the shape.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
