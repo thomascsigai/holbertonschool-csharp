41867f0e7e667787d05eaaac1a1c9f1be9b6bc2e
thomascsigai <thomascsigai@github.com>
2024-01-15 14:08:51 +0100

    Adding task 2 program

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+///<summary> Defines objects tools methods implementations.</summary>
+class Obj
+{
+    ///<summary>Tells if the object is an instance of a class that inherits from the specified class.</summary>
+    ///<param name="derivedType">The subclass.</param>
+    ///<param name="baseType">The base class.</param>
+    ///<returns>True if the object is an instance of a class that inherits from the specified class, otherwise return False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
