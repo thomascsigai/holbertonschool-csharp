c5c32eacf8649c9ae95c38316d6d66638bec4484
thomascsigai <thomascsigai@github.com>
2024-01-15 14:58:57 +0100

    Adding task 6 program

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+///<summary> Defines a Shape.</summary>
+class Shape
+{
+    ///<summary>Return the area of the shape.</summary>
+    ///<returns>The aera of the shape.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+///<summary> Defines a Rectangle, inherits from Shape.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    ///<summary>Gets or sets the shape's width</summary>
+    public int Width 
+    {
+        get { return width; } 
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            else
+                width = value;
+        }
+    }
+
+    ///<summary>Gets or sets the shape's height</summary>
+    public int Height 
+    {
+        get { return height; } 
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            else
+                height = value;
+        }
+    }
+}
