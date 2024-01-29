2aa422292f35bb436faffc0236b513c850b562a4
thomascsigai <thomascsigai1@gmail.com>
2024-01-29 19:31:20 +0100

    Adding task 18 program

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+///<summary>Defines matrix calculation methods.</summary>
+class MatrixMath
+{
+    ///<summary>Multiplies two matrices.</summary>
+    ///<param name="matrix1">The first matrix.</param>
+    ///<param name="matrix2">The second matrix.</param>
+    ///<returns>The resulting matrix.</returns>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        //Check if matrices can be multiplied
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return new double[,] { { -1 } };
+        
+        double[,] resultMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                resultMatrix[i, j] = 0;
+
+                for (int k = 0; k < matrix1.GetLength(1); k++)
+                {
+                    resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return resultMatrix;
+    }
+}
