91a30de1b2708a6c3a7b84aa80a2671a034668f1
thomascsigai <thomascsigai1@gmail.com>
2024-01-29 15:04:58 +0100

    Adding task 3 program

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+///<summary>Defines vector calculation methods.</summary>
+class VectorMath
+{
+    ///<summary>Calculates and returns the length of a given vector.</summary>
+    ///<param name="vector">The vector.</param>
+    ///<returns>The length of the vector rounded to the nearest hundredth.</returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Count() != 2 || vector.Count() != 3)
+            return -1;
+
+        double squaredSum = 0;
+        
+        foreach (double number in vector)
+        {
+            squaredSum += number * number;
+        }
+        
+        return Math.Round(Math.Sqrt(squaredSum), 2);
+    }
+}
