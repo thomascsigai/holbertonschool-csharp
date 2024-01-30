41b7b35e78d4639614cb115cf4f4287fedaaa117
thomascsigai <thomascsigai1@gmail.com>
2024-01-30 14:51:21 +0100

    Update task 26 program

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -4,16 +4,12 @@ class MatrixMath
 {
     public static double Determinant(double[,] matrix)
     {
-        //Checks if matrixes are same size
-        if (matrix1.Length - matrix2.Length != 0)
-            return new double[,] { { -1 } };
+        if (matrix.Length != 4 && matrix.Length != 9)
+            return -1;
 
-        if (matrix1.Length != 4 && matrix2.Length != 9)
-            return new double[,] { { -1 } };
-
-        //Checks if matrix1 is 2D or 3D
+        //Checks if matrix is 2D or 3D
         if (matrix.GetLength(0) != 2 && matrix.GetLength(1) != 2 && matrix.GetLength(0) != 3 && matrix.GetLength(1) != 3)
-            return new double[,] { { -1 } };
+            return -1;
 
         if (matrix.Length == 4)
             return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
