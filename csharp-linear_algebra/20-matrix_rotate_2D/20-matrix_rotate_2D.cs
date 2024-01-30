e0b32c933db3345512f582cdf5561a4ac6398345
thomascsigai <thomascsigai1@gmail.com>
2024-01-30 12:30:23 +0100

    Adding task 20 program

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        // Vérifier si la matrice est carrée
+        if (rows != cols)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] rotatedMatrix = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // Appliquer la rotation à chaque élément
+                rotatedMatrix[i, j] = matrix[i, j] * Math.Cos(angle) - matrix[j, i] * Math.Sin(angle);
+            }
+        }
+
+        return rotatedMatrix;
+    }
+}
