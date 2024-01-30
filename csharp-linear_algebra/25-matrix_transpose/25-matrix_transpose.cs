2144dd45eca6b23370f87ca54a410c10ba6987b9
thomascsigai <thomascsigai1@gmail.com>
2024-01-30 14:33:38 +0100

    Adding task 23 program

---
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+            return new double[,] {};
+
+        double[,] tMat = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                tMat[j, i] = matrix[i, j];
+            }
+        }
+
+        return tMat;
+    }
+}
