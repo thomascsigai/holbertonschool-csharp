de9440f09b352e5a0af1ce91e9b2a26801b4ac69
thomascsigai <thomascsigai1@gmail.com>
2024-02-15 14:54:23 +0100

    Adding task 0 program

---
+++ image_processor/ImageProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+class ImageProcessor
+{
+    public static void Inverse(string[] filenames)
+    {
+        foreach (string file in filenames)
+        {
+            Bitmap originalBitmap = new Bitmap(file);
+        }
+    }
+}
