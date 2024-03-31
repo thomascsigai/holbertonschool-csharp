fde48f9988f7d0dbd12ee35e5e31a2f80b4f79ff
thomascsigai <thomascsigai1@gmail.com>
2024-03-31 18:15:29 +0200

    Adding task 0 file

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Base class</summary>
+abstract class Base
+{
+  public string name = "";
+
+	public override string ToString()
+	{
+		return (this.name + " is a " + this.GetType().ToString());
+	}
+}
