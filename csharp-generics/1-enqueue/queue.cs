d170965aa7eed74fa352cce29328c37e5e7d7911
thomascsigai <thomascsigai1@gmail.com>
2024-03-31 18:02:16 +0200

    Adding task 1 program

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+///<summary>Class for que opperations</summary>
+/// <typeparam name="T">param type</typeparam>
+class Queue<T>
+{
+
+    ///<summary>Check if its a queue type</summary>
+	///<return>returns a Queue type</return>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+	
+    
+     /// <summary>Class Node inside queue class</summary>
+	public class Node
+	{
+		private T value;
+		public Node next = null;
+
+		public Node(T stri)
+		{
+			value = stri;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+	public int count;
+
+	///<summary>Add new node at the end</summary>
+	public void Enqueue(T value)
+	{
+		Node newnode = new Node(value);
+		if (head == null)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail.next = newnode;
+			tail = newnode;
+		}
+		count++;
+	}
+
+	///<summary>Add new node at the end</summary>
+	public int Count()
+	{
+		return count;
+	}
+
+
+}
